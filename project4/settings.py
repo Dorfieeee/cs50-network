ca26ac05120a048f62d65263da220db80c9a8b70
Dorfieeee <rosendorf.dev@gmail.com>
2021-08-13 13:43:16 +0100

    heroku prep

--- project4/settings.py
+++ project4/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['peep-er.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tweeeeeeeeter.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
